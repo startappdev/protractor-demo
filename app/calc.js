55aa1fec0c523b9d3c57f79361d517ae110fb0ea
Matt Steele <orphum@gmail.com>
2015-07-27 21:31:34 -0500

    Extract addition to prototype function

--- app/calc.js
+++ app/calc.js
@@ -2,56 +2,51 @@ var CalcCtrl = function($timeout) {
   var calc = this;
   calc.memory = [];
   calc.latest = 0;
-  calc.operators = {
-    ADDITION: '+',
-    SUBTRACTION: '-',
-    MULTIPLICATION: '*',
-    DIVISION: '/',
-    MODULO: '%'
-  };
-  calc.operator = calc.operators.ADDITION;
+  calc.operator = "0";
+  calc.$timeout = $timeout;
+}
 
-  calc.doAddition = function() {
-    var times = 5;
-    calc.latest = '. ';
-    $timeout(function tickslowly() {
-      if (times == 0) {
-        var latestResult;
-        var first = parseInt(calc.first);
-        var second = parseInt(calc.second);
-        switch (calc.operator) {
-          case '+':
-            latestResult = first + second;
-            break;
-          case '-':
-            latestResult = first - second;
-            break;
-          case '*':
-            latestResult = first * second;
-            break;
-          case '/':
-            latestResult = first / second;
-            break;
-          case '%':
-            latestResult = first % second;
-            break;
-        }
-        calc.memory.unshift({
-          timestamp: new Date(),
-          first: calc.first,
-          operator: calc.operator,
-          second: calc.second,
-          value: latestResult
-        });
-        calc.first = calc.second = '';
-        calc.latest = latestResult;
-      } else {
-        calc.latest += '. ';
-        times--;
-        $timeout(tickslowly, 300);
+CalcCtrl.prototype.doAddition = function() {
+  var calc = this;
+  var times = 5;
+  calc.latest = '. ';
+  calc.$timeout(function tickslowly() {
+    if (times == 0) {
+      var latestResult;
+      var first = parseInt(calc.first);
+      var second = parseInt(calc.second);
+      switch (calc.operator) {
+        case "0":
+          latestResult = first + second;
+          break;
+        case "1":
+          latestResult = first - second;
+          break;
+        case "2":
+          latestResult = first * second;
+          break;
+        case "3":
+          latestResult = first / second;
+          break;
+        case "4":
+          latestResult = first % second;
+          break;
       }
-    }, 300)
-  };
+      calc.memory.unshift({
+        timestamp: new Date(),
+        first: calc.first,
+        operator: calc.operator,
+        second: calc.second,
+        value: latestResult
+      });
+      calc.first = calc.second = '';
+      calc.latest = latestResult;
+    } else {
+      calc.latest += '. ';
+      times--;
+      calc.$timeout(tickslowly, 300);
+    }
+  }, 300)
 };
 
 var calculator = angular.module('calculator', []).
