e83a5717472a6b2fd2b314aef120da896f690129
nadavsinai <ns@nadavsinai.com>
2015-08-30 11:00:38 +0300

    add simple example

--- test/spec.js
+++ test/spec.js
@@ -4,6 +4,14 @@ describe('slow calculator', function () {
     });
 
     it('should add numbers', function () {
+        element(by.model('first')).sendKeys(4);
+        element(by.model('second')).sendKeys(5);
+        element(by.id('gobutton')).click();
+        expect(element(by.binding('latest')).getText()).
+            toEqual('9');
+    });
+
+    it('should verify all simple math operations exist in the dropdown menu (+,-,*,/)', function () {
 
     });
 
@@ -19,13 +27,13 @@ describe('slow calculator', function () {
         it('should only display the last 5 results window', function () {
 
         })
-        
+
     });
-    
-    describe('check special functions',function(){
-        
+
+    describe('check special functions', function () {
+
         it('should test that results that equal 42 are colored red', function () {
-            
+
         });
 
         it('should print NaN with link to wikipedia (https://en.wikipedia.org/wiki/NaN) if result is NaN', function () {
